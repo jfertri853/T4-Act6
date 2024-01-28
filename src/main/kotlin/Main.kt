9bfc9d21feeb34a2a5fa89bff404cfe17a90a85a
jfertri853 <ftjulio@iesrafaelalberti.es>
2024-01-28 23:34:17 +0100

    Inicio

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+/*
+Realizar el ejercicio 1 de Conjuntos de los "Ejercicios básicos con Kotlin" (Ejercicio 3.3.1) orientado a objetos.
+
+La clase Domicilio tendrá un método llamado dirCompleta() que retornará el domicilio completo con la calle y el número.
+
+Las clases Compra, Cliente y Domicilio se establecerán como data class, es decir,
+ delante de class incluirán el modificador data en la declaración de dichas clases.
+
+La clase RepositorioCompras tendrá un método para agregar una compra al repositorio y un método domicilios
+ para retornar los domicilios de cada cliente al cual se le debe enviar una factura de compra.
+ Esta función retorna una estructura que contenga cada domicilio una sola vez.
+ */
+
+package org.example
+
+fun main() {
+}
